1f8627e273088ba6974977e999f81cbc11dcc821
David P. Sicilia <dpsicilia@gmail.com>
2018-11-20 21:30:15 -0500

    Move net.cpp to catch2

--- test/net.cpp
+++ test/net.cpp
@@ -1,26 +1,26 @@
 /****************************************************************
 * Unit tests for network functionality
 ****************************************************************/
-#include "common.hpp"
+#include "common-catch.hpp"
 
 #include "base-util/bimap.hpp"
 #include "base-util/io.hpp"
 #include "base-util/net.hpp"
 #include "base-util/types.hpp"
 
+#include "catch2/catch.hpp"
+
 using namespace std;
 
-namespace testing {
-
-TEST( url_encode )
+TEST_CASE( "url_encode" )
 {
     auto s_inp  = util::read_file_str( data_common/"encode-inp.txt"  );
     auto s_base = util::read_file_str( data_common/"encode-base.txt" );
 
     auto s_enc = net::url_encode( s_inp );
 
-    EQUALS( s_enc.size(), s_base.size() );
-    EQUALS( s_enc, s_base );
+    REQUIRE( s_enc.size() == s_base.size() );
+    REQUIRE( s_enc == s_base );
 
     // Test encoding of key/value pairs from vector.
     vector<tuple<string, string>> kv1{
@@ -36,7 +36,7 @@ TEST( url_encode )
         "with%20spaces=with%26amp&"
         "empty-val=";
 
-    EQUALS( net::url_encode_kv( kv1 ), target );
+    REQUIRE( net::url_encode_kv( kv1 ) == target );
 
     // Test another, map-like container.
     util::BiMapFixed<string, string> bm( move( kv1 ) );
@@ -47,7 +47,5 @@ TEST( url_encode )
              "hello=world&"
              "with%20spaces=with%26amp";
 
-    EQUALS( net::url_encode_kv( bm ), target );
+    REQUIRE( net::url_encode_kv( bm ) == target );
 }
-
-} // namespace testing
